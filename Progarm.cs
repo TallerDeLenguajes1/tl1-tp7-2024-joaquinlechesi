7912ea1137537ed9e7ee72b05d0af195fad7ca18
joaquinlechesi <joaquinlechesi@gmail.com>
2024-06-10 21:13:22 -0300

    Ejercicio 1

--- Progarm.cs
+++ Progarm.cs
@@ -13,7 +13,7 @@ double numeroDouble;
 
 do
 {
-    Console.WriteLine("Ingrese una opcion:");
+    Console.WriteLine("Ingrese una opcion: 1 para operacion SUMA, 2 para operacion RESTA, 3 para la operacion MULTIPLICACION, 4 para la operacion division.");
     opcion = Console.ReadLine();
     int.TryParse(opcion, out opcionInt);
     switch (opcionInt)
@@ -23,6 +23,24 @@ do
             numero = Console.ReadLine();
             double.TryParse(numero, out numeroDouble);
             NuevaCalculadora.Sumar(numeroDouble);
+            break;
+        case 2:
+            Console.WriteLine("Ingrese un numero para realizar una resta.");
+            numero = Console.ReadLine();
+            double.TryParse(numero, out numeroDouble);
+            NuevaCalculadora.Restar(numeroDouble);
+            break;
+        case 3:
+            Console.WriteLine("Ingrese un numero para realizar una multiplicacion.");
+            numero = Console.ReadLine();
+            double.TryParse(numero, out numeroDouble);
+            NuevaCalculadora.Mulpiplicar(numeroDouble);
+            break;
+        case 4:
+            Console.WriteLine("Ingrese un numero para realizar una divicion.");
+            numero = Console.ReadLine();
+            double.TryParse(numero, out numeroDouble);
+            NuevaCalculadora.Dividir(numeroDouble);
             break;
         default:
             break;
