9938b720b40fdf26f38afdb3c86abc9036d2b49a
joaquinlechesi <joaquinlechesi@gmail.com>
2024-06-10 22:00:13 -0300

    Ejercicio 2

---
+++ Empleado.cs
@@ -0,0 +1,11 @@
+namespace EspacioEmpleado;
+
+public class Empleado {
+    private string Nombre;
+    private string Apellido;
+    private DateTime FechaNacimiendo;
+    private char EstadoCivil;
+    private DateTime FechaIngreso;
+    private double SueldoBasico;
+    
+}
